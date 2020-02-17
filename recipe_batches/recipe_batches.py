2d44eaef82e064e41b2d0d9821e2f2b4746f3ff7
Cooper Williams <strangelycutlemon@gmail.com>
2020-02-16 23:51:57 -0500

    find max # batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
-
+    max_batches = float("inf")
+    for i, j in recipe.items():
+        try:
+            max_for_ingredient = ingredients[i] // j
+        except KeyError:
+            # if a required ingredient is missing, 0 batches can be made
+            return 0
+        # max batches limited to the least available ingredient
+        max_batches = min(max_batches, max_for_ingredient)
+    return max_batches
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
