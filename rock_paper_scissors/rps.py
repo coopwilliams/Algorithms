eefe31bc0d11742bed235c478f0e4afd69706910
Cooper Williams <strangelycutlemon@gmail.com>
2020-02-17 11:14:13 -0500

    count rock paper scissors plays

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,21 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
-
+    list_length_n = {0:[[]], 1:[['rock'], ['paper'], ['scissors']]}
+    options = ['rock', 'paper', 'scissors']
+    def rock_paper_scissors_iter(n):
+        nonlocal list_length_n, options
+        # use cache if list already calculated
+        if n in list_length_n.keys():
+            return list_length_n[n]
+        new_list = []
+        # each play from n - 1 can now start with one of three options
+        for i in options:
+            new_list.extend([[i] + x for x in rock_paper_scissors_iter(n - 1)])
+        # update cache
+        list_length_n[n] = new_list
+        return new_list
+    return rock_paper_scissors_iter(n)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
