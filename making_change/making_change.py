fb3c62af414dce95442438fdd1e66cf6571cc5e2
Cooper Williams <strangelycutlemon@gmail.com>
2020-02-17 12:05:52 -0500

    count ways to make change for arbitrary denoms

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,41 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+def making_change(amount, n_coins):
+    def cc(amount, n_coins=5):
+        """Count ways to make change with up to 5 coins"""
+        if amount == 0: return 1
+        elif (amount < 0) or (n_coins == 0): return 0
+        else:
+            return  cc(amount, n_coins - 1) + \
+                    cc(amount - first_denomination[n_coins], n_coins)
 
+    def cc_missing_denominations(amount, n_coins):
+        """Count ways to make change with gaps in the denominations.
+                E.g. pennies and dimes only (no nickels).
+        """
+        if amount == 0: return 1
+        elif (amount < 0) or (len(n_coins) == 0): return 0
+        else:
+            return  cc_missing_denominations(amount, sorted(n_coins)[:-1]) + \
+                    cc_missing_denominations(amount - max(n_coins), n_coins)
+
+    first_denomination = {1:1, 2:5, 3:10, 4:25, 5:50}
+    # check there are no gaps in the denominations
+    if n_coins == list(first_denomination.values())[:len(n_coins) - 1]:
+        n_coins = len(n_coins)
+        return cc(amount, n_coins)
+    else:
+        return cc_missing_denominations(amount, n_coins)
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
   # with `python making_change.py [amount]` with different amounts
   if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
+    # extra arguments can be passed to specify arbitrary denominations
+    denominations = [int(x) for x in sys.argv[2:len(sys.argv)]] \
+                    or [1, 5, 10, 25, 50]
     amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+    print("There are {ways} ways to make {amount} cents with denominations {denominations}.".format(ways=making_change(amount, denominations), amount=amount, denominations=denominations))
   else:
     print("Usage: making_change.py [amount]")
